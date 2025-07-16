9a8058996943158f4a55fecbe8d8e2d7e44375b0
Samuele <samuele.vignoli08@gmail.com>
2025-07-16 17:09:10 +0200

    no_diff

---
+++ AdminUser.java
@@ -0,0 +1,12 @@
+public class AdminUser{
+     String username;
+     String password;
+     String token;
+
+    public AdminUser(String username, String password, String token){
+        this.username = username;
+        this.password = password;
+        this.token = token;
+        
+    }
+} 
